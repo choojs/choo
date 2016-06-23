51a9c4488e79aee8a43cd91ca64cb144760c845b
zigomir <zigomir@gmail.com>
2016-06-22 17:39:13 -0700

    Fix full reload on specific message.

--- examples/mailbox/server.js
+++ examples/mailbox/server.js
@@ -11,13 +11,14 @@ server.listen(PORT, () => process.stdout.write(`listening on port ${PORT}\n`))
 function createRouter () {
   const router = serverRouter('/404')
 
+  const js = bankai.js(browserify, require.resolve('./client.js'))
+  router.on('/bundle.js', (req, res) => js(req, res).pipe(res))
+  router.on('/:inbox/bundle.js', (req, res) => js(req, res).pipe(res))
+
   const html = bankai.html({ css: false })
   router.on('/', (req, res) => html(req, res).pipe(res))
   router.on('/:inbox', (req, res) => html(req, res).pipe(res))
   router.on('/:inbox/:message_id', (req, res) => html(req, res).pipe(res))
-
-  const js = bankai.js(browserify, require.resolve('./client.js'))
-  router.on('/bundle.js', (req, res) => js(req, res).pipe(res))
 
   router.on('/hi', (req, res) => res.end('{ "message": "hi back!" }'))
   router.on('/404', (req, res) => {
