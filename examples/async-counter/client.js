9fb124cf1cfc7671a3ae81c09d9013d1e21ee10e
Juan Soto <juan@juansoto.me>
2016-07-09 14:03:09 -0400

    Pluralize "times" correctly

--- examples/async-counter/client.js
+++ examples/async-counter/client.js
@@ -1,5 +1,6 @@
 const choo = require('../../')
 const html = require('../../html')
+const plur = require('plur')
 
 const app = choo()
 app.model({
@@ -21,10 +22,12 @@ app.model({
 })
 
 const mainView = (state, prev, send) => {
+  const count = state.counter;
+
   return html`
     <main class="app">
       <h1>Async counter</h1>
-      <p>Clicked ${state.counter} times!</p>
+      <p>Clicked ${count} ${plur('time', count)}!</p>
       <button onclick=${() => send('increment')}>Increment</button>
       <button onclick=${() => send('decrement')}>Decrement</button>
       <button onclick=${() => send('incrementAsync')}>Increment async</button>
