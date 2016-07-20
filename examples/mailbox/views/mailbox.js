bddb20cdad333356562bcacfe1b9e8c31c733f3d
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-07-20 16:53:29 +0200

    location: change url on location:setLocation

--- examples/mailbox/views/mailbox.js
+++ examples/mailbox/views/mailbox.js
@@ -7,6 +7,7 @@ const nav = require('../elements/nav')
 module.exports = function (state, prev, send) {
   return html`
     <main class="mw5 mw7-ns center cf">
+      <button onclick=${goHome}>Home</button>
       ${pathname(state, prev, send)}
       ${nav(state, prev, send)}
       <section class="fl mt4 w-80 db">
@@ -14,4 +15,8 @@ module.exports = function (state, prev, send) {
       </section>
     </main>
   `
+
+  function goHome () {
+    send('location:setLocation', { location: '/'})
+  }
 }
--- index.js
+++ index.js
@@ -142,8 +142,19 @@ function appInit (opts) {
   const loc = document.location
   const state = { pathname: (opts.hash) ? hashMatch(loc.hash) : loc.href }
   const reducers = {
-    setLocation: function setLocation (data, state) {
-      return { pathname: data.location.replace(/#.*/, '') }
+    updateHref: function (data, state) {
+      return { pathname: data }
+    }
+  }
+  const effects = {
+    setLocation: function setLocation (data, state, send, done) {
+      const href = data.location.replace(/#.*/, '')
+      if (window.location.pathname !== href) {
+        window.history.pushState({}, null, href)
+        send('location:updateHref', href, done)
+      } else {
+        done()
+      }
     }
   }
   // if hash routing explicitly enabled, subscribe to it
@@ -163,6 +174,7 @@ function appInit (opts) {
     namespace: 'location',
     subscriptions: subs,
     reducers: reducers,
+    effects: effects,
     state: state
   }
 
