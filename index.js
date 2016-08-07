a19e3e35770d5ac6b50902b99d27950d41e92b43
Yoshua Wuyts <yoshuawuyts@gmail.com>
2016-08-07 17:32:59 +0200

    fixup! location: update

--- index.js
+++ index.js
@@ -131,40 +131,45 @@ function choo (opts) {
 // application location model
 // obj -> obj
 function createLocationModel (opts) {
-  const subs = {}
-  if (opts.history !== false) {
-    subs.handleHistory = function (send, done) {
-      onHistoryChange(function navigate (pathname) {
-        send('location:set', { pathname: pathname }, done)
-      })
-    }
-  }
-
-  if (opts.href !== false) {
-    subs.handleHref = function (send, done) {
-      onHref(function navigate (pathname) {
-        send('location:set', { pathname: pathname }, done)
-      })
-    }
-  }
-
   return {
     namespace: 'location',
     state: createLocation(null, document.location),
-    subscriptions: subs,
-    reducers: {
-      update: function (location, state) {
-        return location
-      }
-    },
-    effects: {
-      set: function setLocation (patch, state, send, done) {
-        const location = createLocation(state, patch)
-        if (opts.history !== false && location.href !== state.href) {
-          window.history.pushState({}, null, location.href)
-        }
-        send('location:update', location, done)
+    subscriptions: createSubscriptions(opts),
+    effects: { set: setLocation },
+    reducers: { update: update }
+  }
+
+  function update (location, state) {
+    return location
+  }
+
+  function setLocation (patch, state, send, done) {
+    const location = createLocation(state, patch)
+    if (opts.history !== false && location.href !== state.href) {
+      window.history.pushState({}, null, location.href)
+    }
+    send('location:update', location, done)
+  }
+
+  function createSubscriptions (opts) {
+    const subs = {}
+
+    if (opts.history !== false) {
+      subs.handleHistory = function (send, done) {
+        onHistoryChange(function navigate (pathname) {
+          send('location:set', { pathname: pathname }, done)
+        })
       }
     }
+
+    if (opts.href !== false) {
+      subs.handleHref = function (send, done) {
+        onHref(function navigate (pathname) {
+          send('location:set', { pathname: pathname }, done)
+        })
+      }
+    }
+
+    return subs
   }
 }
