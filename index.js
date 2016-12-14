194cb7a0b4efcf427e933b748d03b8238f5da0e3
Arve Knudsen <arve.knudsen@gmail.com>
2016-12-14 15:33:30 +0100

    Fix README wrt. modifying location

--- index.js
+++ index.js
@@ -178,7 +178,6 @@ function createLocationModel (opts) {
   // (str, obj, fn, fn) -> null
   function setLocation (state, data, send, done) {
     const newLocation = createLocation(state, data)
-    console.log(`New location:`, newLocation)
 
     // update url bar if it changed
     if (opts.history !== false && newLocation.href !== state.href) {
