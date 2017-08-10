e20a6b5124ee48ca4288147e49f6065ec782541c
Todd Kennedy <todd@selfassembled.org>
2017-08-09 17:42:59 -0700

    Add typescript definition

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+/// <reference types="node" />
+
+import * as EventEmitter from 'events'
+
+export = Choo
+
+declare class Choo {
+  constructor (opts: Choo.IChoo)
+  use (callback: (state: Choo.IState, emitter: EventEmitter) => void): void
+  route (routeName: string, handler: (state: Choo.IState, emit: (name: string, ...args: any[]) => void) => void): void
+  mount (selector: string): void
+  start (): HTMLElement
+  toString (location: string, state?: Choo.IState): string
+}
+
+declare namespace Choo {
+  export interface IChoo {
+    history?: boolean
+    href?: boolean
+  }
+
+  export interface IState {
+    events: {
+      [key: string]: string
+    }
+    params: {
+      [key: string]: string
+    }
+    query?: {
+      [key: string]: string
+    }
+    href: string
+    route: string
+    title: string
+    [key: string]: any
+  }
+}
+
