6ae949bf0ef02f39e3798aa3d47585fa156176fb
Nate Goldman <ungoldman@gmail.com>
2018-02-23 12:52:54 -0800

    add failing test for hyperscript (#634)

--- test.js
+++ test.js
@@ -1,16 +1,28 @@
 var tape = require('tape')
+var h = require('hyperscript')
 
 var html = require('./html')
 var raw = require('./html/raw')
 var choo = require('./')
 
-tape('should render on the server', function (t) {
+tape('should render on the server with bel', function (t) {
   var app = choo()
   app.route('/', function (state, emit) {
     var strong = '<strong>Hello filthy planet</strong>'
     return html`
       <p>${raw(strong)}</p>
     `
+  })
+  var res = app.toString('/')
+  var exp = '<p><strong>Hello filthy planet</strong></p>'
+  t.equal(res.toString().trim(), exp, 'result was OK')
+  t.end()
+})
+
+tape('should render on the server with hyperscript', function (t) {
+  var app = choo()
+  app.route('/', function (state, emit) {
+    return h('p', h('strong', 'Hello filthy planet'))
   })
   var res = app.toString('/')
   var exp = '<p><strong>Hello filthy planet</strong></p>'
