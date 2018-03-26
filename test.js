919d5d3a3c76515b1c48470e9adf4c2c6d95fe1b
Diogo Cunha <diogo.f.cunha@gmail.com>
2018-03-26 18:34:03 +0100

    few more tests

--- test.js
+++ test.js
@@ -34,6 +34,64 @@ tape('should render async on the server with bel', function (t) {
   })
 })
 
+tape('should render composition of sync and async on the server', function (t) {
+  var app = choo()
+  var async = function (state, emit) {
+    return new Promise(function (resolve) {
+      resolve(html`<p>Hello!</p>`)
+    })
+  }
+  var hoc = function (child) {
+    return function (state, emit) {
+      return html`<div>${child(state, emit)}</div>`
+    }
+  }
+  var component = hoc(async)
+  app.route('/', function (state, emit) {
+    return html`<div>${component(state, emit)}</div>`
+  })
+  app.toString('/').then(function (res) {
+    var exp = '<div><div><p>Hello!</p></div></div>'
+    t.equal(res.toString().trim(), exp, 'result was OK')
+    t.end()
+  })
+})
+
+tape('should render async with custom promise resolution strategy', function (t) {
+  t.plan(5)
+  var order = 3
+  var app = choo()
+  var component1 = function (state, emit) {
+    return new Promise(function (resolve) { resolve(html`<p>hEllo!</p>`) })
+  }
+  var component2 = function (state, emit) {
+    return new Promise(function (resolve) { resolve(html`<p>heLlo!</p>`) })
+  }
+  var component3 = function (state, emit) {
+    return new Promise(function (resolve) { resolve(html`<p>helLo!</p>`) })
+  }
+  app.route('/', function (state, emit) {
+    // resolve component3 first then component2 and then component1
+    var pC3 = component3(state, emit).then(function (result) { t.equal(order, 3); order--; return result })
+    var pC2 = pC3.then(function () { return component2(state, emit).then(function (result) { t.equal(order, 2); order--; return result }) })
+    var pC1 = pC2.then(function () { return component1(state, emit).then(function (result) { t.equal(order, 1); order--; return result }) })
+    return html`
+      <div>
+        ${pC1}
+        ${pC2}
+        ${pC3}
+        ${'HELLO'}
+      </div>
+    `
+  })
+  app.toString('/').then(function (res) {
+    var exp = '<div>\n        <p>hEllo!</p>\n        <p>heLlo!</p>\n        <p>helLo!</p>\n        HELLO\n      </div>'
+    t.equal(order, 0, 'order was OK')
+    t.equal(res.toString().trim(), exp, 'result was OK')
+    t.end()
+  })
+})
+
 tape('should render on the server with hyperscript', function (t) {
   var app = choo()
   app.route('/', function (state, emit) {
